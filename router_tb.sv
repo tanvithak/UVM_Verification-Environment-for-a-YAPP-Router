b7b87641b7053552db675425abb3c25e929c58d8
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-09-13 21:38:33 -0700

    Update router_tb.sv

--- router_tb.sv
+++ router_tb.sv
@@ -3,9 +3,12 @@ class router_tb extends uvm_env;
   function new(string name="router_tb", uvm_component parent);
     super.new(name,parent);
   endfunction
-
+  
+  yapp_env env;
+  
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     `uvm_info(get_type_name(),$sformatf("Testbench build phase executed"),UVM_HIGH)
+    env = yapp_env::type_id::create("env",this);
   endfunction
 endclass
