ed870f001d33537f270b32b901a12457db639181
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-09-15 16:40:18 -0700

    Update yapp_tx_monitor.sv

--- yapp_tx_monitor.sv
+++ yapp_tx_monitor.sv
@@ -1,11 +1,53 @@
 class yapp_tx_monitor extends uvm_monitor;
-  `uvm_component_utils(yapp_tx_monitor)
+  
+  virtual interface yapp_if vif;
+
+  `uvm_component_utils_begin(yapp_tx_monitor)
+    `uvm_field_int(num_pkt_col, UVM_ALL_ON)
+  `uvm_component_utils_end
 
   function new(string name="yapp_tx_monitor",uvm_component parent);
     super.new(name,parent);
   endfunction
 
+  yapp_packet pkt;
+
+  // Count packets collected
+  int num_pkt_col;
+
+  virtual function void connect_phase(uvm_phase phase);
+    if (!yapp_vif_config::get(this,get_full_name(),"vif", vif))
+     `uvm_error("NOVIF",{"virtual interface must be set for: ",get_full_name(),".vif"})
+  endfunction
+
+  // UVM run() phase
   virtual task run_phase(uvm_phase phase);
-    `uvm_info(get_type_name(),$sformatf("In monitor's run_phase"),UVM_LOW)
-  endtask
+    // Look for packets after reset
+    @(posedge vif.reset)
+    @(negedge vif.reset)
+    `uvm_info(get_type_name(), "Detected Reset Done", UVM_MEDIUM)
+    forever begin 
+      // Create collected packet instance
+      pkt = yapp_packet::type_id::create("pkt", this);
+
+      // concurrent blocks for packet collection and transaction recording
+      fork
+        // collect packet
+        vif.collect_packet(pkt.length, pkt.addr, pkt.payload, pkt.parity);
+        // trigger transaction at start of packet
+        @(posedge vif.monstart) void'(begin_tr(pkt, "Monitor_YAPP_Packet"));
+      join
+
+      pkt.parity_type = (pkt.parity == pkt.calc_parity()) ? GOOD_PARITY : BAD_PARITY;
+      // End transaction recording
+      end_tr(pkt);
+      `uvm_info(get_type_name(), $sformatf("Packet Collected :\n%s", pkt.sprint()), UVM_LOW)
+      num_pkt_col++;
+    end
+  endtask : run_phase
+
+  // UVM report_phase
+  virtual function void report_phase(uvm_phase phase);
+    `uvm_info(get_type_name(), $sformatf("Report: YAPP Monitor Collected %0d Packets", num_pkt_col), UVM_LOW)
+  endfunction : report_phase
 endclass
